19194ba1e997cc4c93dd7b040677a69dd1ac1c7a
Raphael Mueller <raaphii@gmail.com>
2016-04-28 19:34:09 +0200

    Fixed issue - files should be available on root address instead of /public

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var http = require('http');
 var publicFolder = path.join(__dirname, 'public');
 
 var app = express();
-app.use('/public', express.static(publicFolder));
+app.use('/', express.static(publicFolder));
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 
 var server = http.createServer(app);
