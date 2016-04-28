803cde567235628417dec37c22bca9d2f800cfef
David Flury <david@flury.email>
2016-04-28 11:26:51 +0200

    Set states active in designer Design changes Machine refactoring Removed unnecessary column generating

--- scripts/tm.column.js
+++ scripts/tm.column.js
@@ -4,26 +4,34 @@ function Column() {
 Column.prototype.read = function() {
     var value = this.input.val();
     return value === "" ? " " : value;
-}
-Column.prototype.write = function(value, tape, interupt) {
+};
+
+Column.prototype.write = function(value, tape, interupt, next) {
     if($(this.element).is(':last-child')) {
-        tape.addColumn();
+        if(!next || next == 'R') {
+            tape.addColumn();
+        }
     }
     if($(this.element).is(':first-child')) {
-        tape.addColumn(true);
-        $.autotab.previous();
+        if(!next || next == 'L') {
+            tape.addColumn(true);
+            $.autotab.previous();
+        }
     }
     if(interupt) {
         self.machine.reset();
     }
     this.input.val(value);
-}
+};
+
 Column.prototype.activate = function() {
     this.readerwriterCell.addClass('active');
-}
+};
+
 Column.prototype.deactivate = function() {
     this.readerwriterCell.removeClass('active');
-}
+};
+
 Column.prototype.getMarkup = function() {
     return this.element.append(this.inputCell.append(this.input)).append(this.readerwriterCell);
-}
+};
--- scripts/tm.machine.js
+++ scripts/tm.machine.js
@@ -2,29 +2,31 @@ function Machine() {
     var self = this;
     this.tape = new Tape(this);
     this.reset();
-    this.tape.move(null, 0);
+    this.tape.position(0);
 }
 
 Machine.prototype.reset = function() {
     this.stop();
     this.result = null;
     this.setConfiguration();
-    this.currentState = this.configuration.startState;
     this.tape.reset();
-}
+    $(".w").removeClass("failed");
+    $(".w").removeClass("accepted");
+    this.setCurrentState(this.configuration.startState);
+};
 
 Machine.prototype.stop = function() {
     clearInterval(this.calculationInterval);
     this.calculationInterval = null;
     this.running = false;
-}
+};
 
 Machine.prototype.calculateAll = function(interval) {
     var self = this;
     this.calculationInterval = setInterval(function(){
         self.calculateStep();
     }, interval);
-}
+};
 
 Machine.prototype.calculateStep = function() {
     this.running = true;
@@ -33,18 +35,29 @@ Machine.prototype.calculateStep = function() {
     }
     var step = this.configuration.states[this.currentState].connections[this.tape.read()];
     if(step) {
-        this.tape.write(step.write);
+        this.tape.write(step.write, step.move);
         this.tape.move(step.move == 'L' ? -1 : step.move == 'R' ? 1 : 0);
-        this.currentState = step.newState;
+        this.setCurrentState(step.newState);
     } else {
-        this.tape.setFinished(this.configuration.states[this.currentState].accepted);
-        this.stop();
-        this.result = this.currentState.accepted;
-        return this.result;
+        return this.setFinished(this.configuration.states[this.currentState].accepted);
     }
     return null;
+};
+
+Machine.prototype.setCurrentState = function(newState) {
+    $("#" + this.currentState).removeClass("active");
+    this.currentState = newState;
+    $("#" + this.currentState).addClass("active");
+};
+
+Machine.prototype.setFinished = function(status) {
+    this.result = status;
+    this.tape.setFinished(status);
+    this.stop();
+    $("#" + this.currentState).addClass(status ? "accepted" : "failed");
+    return this.result;
 }
 
 Machine.prototype.setConfiguration = function() {
     this.configuration = configuration;
-}
+};
