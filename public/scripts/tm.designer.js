b7410d66a929d75b58df7b551862f46f00b68326
Raphael Mueller <raaphii@gmail.com>
2016-04-30 08:51:54 +0200

    Fixed syntax error

--- public/scripts/tm.designer.js
+++ public/scripts/tm.designer.js
@@ -1,6 +1,6 @@
 // Enable persistent configuration
 var localAutoSave = JSON.parse(localStorage.configuration);
-var configuration = localAutoSave != null && localAutoSave != undefined ? localAutoSave  || {states: {}};
+var configuration = localAutoSave != null && localAutoSave != undefined ? localAutoSave : {states: {}};
 var MachineCanvas = function () {
 
     var self = this;
